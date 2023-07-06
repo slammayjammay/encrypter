89235e622e976480b00c09f4f32bf88660877532
Scott McKenzie <slammayjammay@gmail.com>
2023-07-06 13:21:42 -0700

    Do not reuse salt and refactor api arguments

--- Encrypter.js
+++ Encrypter.js
@@ -1,35 +1,30 @@
 import * as cryptoApi from './crypto-api.js';
 
 export default class Encrypter {
-	constructor(crypto, salt = this.randomSalt()) {
+	constructor(crypto) {
 		this.crypto = crypto;
-		this.salt = salt;
 	}
 
-	randomSalt() {
-		return cryptoApi.encode(new Array(10).fill(null).map(() => `${Math.random()}`.slice(2)).join(''));
+	async encrypt(string, password, crypto = this.crypto) {
+		const { iv, cipher, salt } = await cryptoApi.encryptFromPassword(crypto, string, password);
+		return cryptoApi.toHex(iv, salt, cipher);
 	}
 
-	async encrypt(string, password, salt = this.salt, crypto = this.crypto) {
-		const { cipher, iv } = await cryptoApi.encryptFromPassword(string, password, salt, crypto);
-		return cryptoApi.toHex(iv, cipher);
-	}
-
-	async decrypt(string, password, salt = this.salt, crypto = this.crypto) {
+	async decrypt(string, password, crypto = this.crypto) {
 		const [error, parsed] = cryptoApi.parseHex(string);
 		if (error) {
 			return [error];
 		}
 
-		const { cipher, iv } = parsed;
+		const { iv, salt, cipher } = parsed;
 		return new Promise(resolve => {
-			cryptoApi.decryptFromPassword(cipher, password, iv, salt, crypto).then(decrypted => {
+			cryptoApi.decryptFromPassword(crypto, cipher, password, iv, salt).then(decrypted => {
 				resolve([null, cryptoApi.decode(decrypted)]);
 			}).catch(e => resolve([e]));
 		});
 	}
 
 	destroy() {
-		this.crypto = this.salt = null;
+		this.crypto = null;
 	}
 }
--- crypto-api.js
+++ crypto-api.js
@@ -7,9 +7,16 @@ export const arrayToTyped = (array, Typed = Uint8Array) => Typed.from(array);
 export const typedToArray = (typed) => Array.from(typed);
 export const bufferToArray = (typed, Typed = Uint8Array) => Array.from(new Typed(typed));
 
-export const encrypt = async (data, key, iv, crypto) => {
+export const ivLength = 12
+export const saltLength = 16
+
+export const getRandomValues = (crypto, n = saltLength) => {
+	return crypto.getRandomValues(new Uint8Array(n));
+}
+
+export const encrypt = async (crypto, data, key, iv) => {
 	data = data instanceof Uint8Array ? data : encode(data);
-	iv = iv || crypto.getRandomValues(new Uint8Array(12));
+	iv = iv || getRandomValues(crypto, ivLength);
 
 	const cipher = await crypto.subtle.encrypt(
 		{ name: 'AES-GCM', iv },
@@ -20,7 +27,7 @@ export const encrypt = async (data, key, iv, crypto) => {
 	return { cipher, iv };
 }
 
-export const decrypt = async (data, key, iv, crypto) => {
+export const decrypt = async (crypto, data, key, iv) => {
 	return crypto.subtle.decrypt(
 		{ name: 'AES-GCM', iv },
 		key,
@@ -28,7 +35,7 @@ export const decrypt = async (data, key, iv, crypto) => {
 	);
 }
 
-export const generateKey = async (data, salt, crypto) => {
+export const generateKey = async (crypto, data, salt) => {
 	data = data instanceof Uint8Array ? data : encode(data);
 
 	const imported = await crypto.subtle.importKey(
@@ -48,22 +55,23 @@ export const generateKey = async (data, salt, crypto) => {
 	);
 }
 
-export const encryptFromPassword = async (string, password, salt, crypto) => {
-	const key = await generateKey(password, salt, crypto);
-	return encrypt(string, key, undefined, crypto);
+export const encryptFromPassword = async (crypto, string, password, salt = getRandomValues(crypto)) => {
+	const key = await generateKey(crypto, password, salt);
+	return { ...await encrypt(crypto, string, key), salt };
 }
 
-export const decryptFromPassword = async (data, password, iv, salt, crypto) => {
-	const key = await generateKey(password, salt, crypto);
-	return decrypt(data, key, iv, crypto);
+export const decryptFromPassword = async (crypto, data, password, iv, salt) => {
+	const key = await generateKey(crypto, password, salt);
+	return decrypt(crypto, data, key, iv);
 }
 
 export const numberToHex = (n) => n.toString(16).padStart(2, '0');
 
-export const toHex = (iv, cipher) => {
+export const toHex = (iv, salt, cipher) => {
 	iv = iv instanceof Array ? iv : typedToArray(iv);
+	salt = salt instanceof Array ? salt : typedToArray(salt);
 	cipher = cipher instanceof Array ? cipher : bufferToArray(cipher);
-	return [...iv, ...cipher].map(n => numberToHex(n)).join('');
+	return [...iv, ...salt, ...cipher].map(n => numberToHex(n)).join('');
 }
 
 export const parseHex = (string) => {
@@ -71,8 +79,9 @@ export const parseHex = (string) => {
 		const array = string.match(/[\da-f]{2}/gi).map(n => parseInt(n, 16));
 
 		return [null, {
-			iv: arrayToTyped(array.slice(0, 12)),
-			cipher: arrayToTyped(array.slice(12))
+			iv: arrayToTyped(array.slice(0, ivLength)),
+			salt: arrayToTyped(array.slice(ivLength, ivLength + saltLength)),
+			cipher: arrayToTyped(array.slice(ivLength + saltLength))
 		}];
 	} catch(e) {
 		return [e];
